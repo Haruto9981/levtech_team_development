9304d2625866a08965095ef7b5d7f11d10416769
kurosawa2357 <kurosawa23571113@gmail.com>
2023-07-08 16:33:01 +0900

    ちょっと変えました

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -7,8 +7,12 @@ use App\Models\Post;
 use App\Modles\User;
 use App\Models\Task;
 
-class UserController extends Controller
-{
+class UserController extends Controller{
+
+    public function create(){
+        return view('users/create');
+    }
+    
     public function store_period(User $user, Request $request){
         $input = $request['user'];
         $post = fill($input)->save();
