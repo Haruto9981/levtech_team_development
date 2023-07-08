5af8bdaac35577575c84c7f04c8754ef7e14de23
akky0315 <akky.yoppy@gmail.com>
2023-07-08 16:06:35 +0900

    ホーム１

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', [PostController::class, 'home']);
+Route::get('/home', [PostController::class, 'home']);
 Route::post('/posts',  [PostController::class, 'store']);
 Route::get('/posts/create',  [PostController::class, 'create']);
 Route::get('/posts/{post}',  [PostController::class, 'show']);
